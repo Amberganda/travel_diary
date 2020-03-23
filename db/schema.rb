adb8dd8a1437debe0fd8bd23f9b1d823e2d3364a
Amber Taranto <ambzzz@gmail.com>
2020-03-22 18:21:13 -0700

    created location model and updated table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_011058) do
+ActiveRecord::Schema.define(version: 2020_03_23_011756) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "user_id"
