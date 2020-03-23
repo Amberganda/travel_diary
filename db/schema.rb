e5b1a316a27cae28134ac49034cd9c4d89835845
Amber Taranto <ambzzz@gmail.com>
2020-03-22 18:17:32 -0700

    created a review model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_002214) do
+ActiveRecord::Schema.define(version: 2020_03_23_011058) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "location_id"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["location_id"], name: "index_reviews_on_location_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
