90590b5b196970c21e5cbe7a91582cae2d0bcefd
Amber Taranto <ambzzz@gmail.com>
2020-03-22 19:21:58 -0700

    enabled devise and created a home controller to route to correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-    has_secure_password
-    has_many :reviews
-    has_many :locations, through :reviews
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-    
+    has_many :reviews
+    has_many :locations, through: :reviews
+
+
 end
