aead77a3f2da339212da2c24834778d8868e9209
Amber Taranto <ambzzz@gmail.com>
2020-04-11 18:26:51 -0700

    got the activerecord scope working for most_reviews

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,15 @@
 class Location < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+
+    scope :most_reviews, -> { 
+        left_joins(:reviews).
+        group(:id).
+        order('COUNT(reviews.id) DESC').
+        first 
+    }
+    
+
+
+
 end
